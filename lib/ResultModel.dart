e251b0fc609daae92d9239a707e13c621942c09b
Harshil2001 <47895092+Harshil2001@users.noreply.github.com>
2021-04-15 12:48:00 +0530

    Add files via upload

---
+++ lib/ResultModel.dart
@@ -0,0 +1,25 @@
+class ResultModel {
+
+  String name;
+  String quantity;
+  int price;
+  int noOfItem;
+
+  ResultModel(String name, String quantity, int price) {
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
+    noOfItem = 1;
+  }
+
+}
+
+/*class ResultModel {
+
+  String result;
+
+  ResultModel(String result) {
+    this.result = result;
+  }
+
+} */
